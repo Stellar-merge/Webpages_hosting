a1feb109aaef87b2433ed6c31785b2c874887a7f
Stellar-merge <168761027+Stellar-merge@users.noreply.github.com>
2025-09-21 00:50:48 +0530

    Delete frontend directory

--- frontend/src/pages/Dashboard.tsx
+++
@@ -1,5 +0,0 @@
-export default function Dashboard() {
-    return (
-        <div className="text-center">This is Dashboard Page</div>
-    )
-}
--- frontend/src/pages/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return (
-        <div className="text-center p-4 bg-gray-200 bottom-0 w-full">
-            <p>©Powered by Team RAGtags</p>
-        </div>
-    )
-}
